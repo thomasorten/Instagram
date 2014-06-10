83c25149ad00d381b6929d52de566a345335f06e
thomasorten <thomasorten@Thomass-MacBook-Pro.local>
2014-06-10 14:34:39 -0500

    Added User entity

---
+++ Instagram/User.m
@@ -0,0 +1,20 @@
+//
+//  User.m
+//  Instagram
+//
+//  Created by Thomas Orten on 6/10/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@dynamic profilePhoto;
+
++ (id)parseClassName
+{
+    return @"User";
+}
+
+@end
