3c1892b7562bb71ac64cb458d4297e15479a9101
Philipe Castilho <cpcastilho11@outlook.com>
2014-06-12 11:27:26 -0500

    Got the CollectionView to show photos and the search to work

--- Instagram/ViewController.m
+++ Instagram/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "PhotoCollectionViewCell.h"
+#import <Parse/Parse.h>
 
 #define kLatestUpdatekey @"Latest Update"
 
@@ -19,7 +20,8 @@
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) IBOutlet UITableView *myTableView;
 @property NSMutableArray *selectedCellIndexes;
-@property NSArray *searchResults;
+@property NSMutableArray *searchResults;
+@property NSMutableArray *initialArray;
 @end
 
 @implementation ViewController
@@ -32,46 +34,87 @@
     self.selectedCellIndexes = [[NSMutableArray alloc] init];
     self.myTableView.hidden = YES;
 
-    [self loadPhotosInitially];
 }
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self loadPhotosInitially];
 
+}
 - (void)getPhotosByTerm:(NSString *)searchTerm
 {
-    self.imagesArray = [[NSMutableArray alloc] init];
-
-    NSString *searchString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9401ce3d1573537ff059cca44fe122f4&text=%@&content_type=1&extras=url_m&per_page=10&format=json&nojsoncallback=1", searchTerm];
-    NSURL *url = [NSURL URLWithString:searchString];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
-        NSArray *images = [[json objectForKey:@"photos"] objectForKey:@"photo"];
-        for (NSDictionary *imageDictionary in images) {
-            [self.imagesArray addObject:[imageDictionary objectForKey:@"url_m"]];
+    self.searchResults = [NSMutableArray new];
+    PFQuery *query = [PFUser query];
+    [query whereKey:@"username" equalTo:searchTerm];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *searchedUsers, NSError *error)
+    {
+        for(PFUser *user in searchedUsers)
+        {
+            NSString *username = user[@"username"];
+            [self.searchResults addObject:username];
         }
         [self.myTableView reloadData];
 
+
     }];
+
+
+
+
+
+//    self.imagesArray = [[NSMutableArray alloc] init];
+//
+//    NSString *searchString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9401ce3d1573537ff059cca44fe122f4&text=%@&content_type=1&extras=url_m&per_page=10&format=json&nojsoncallback=1", searchTerm];
+//    NSURL *url = [NSURL URLWithString:searchString];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+//        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+//        NSArray *images = [[json objectForKey:@"photos"] objectForKey:@"photo"];
+//        for (NSDictionary *imageDictionary in images) {
+//            [self.imagesArray addObject:[imageDictionary objectForKey:@"url_m"]];
+//        }
+//        [self.myTableView reloadData];
+//
+//    }];
 
 }
 
 - (void)loadPhotosInitially
 {
-    self.imagesArray = [[NSMutableArray alloc] init];
+    self.initialArray = [NSMutableArray new];
+    PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
+    [query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *photos, NSError *error) {
+        for (PFObject *object in photos) {
+            if (object[@"photo"] ) {
+            PFFile *userImageFile = object[@"photo"];
+            [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+                [self.initialArray addObject:[UIImage imageWithData:imageData]];
 
-    NSString *searchTerm = @"Dogs";
-    NSString *searchString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9401ce3d1573537ff059cca44fe122f4&text=%@&content_type=1&extras=url_m&per_page=10&format=json&nojsoncallback=1", searchTerm];
-    NSURL *url = [NSURL URLWithString:searchString];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
-        NSArray *images = [[json objectForKey:@"photos"] objectForKey:@"photo"];
-        for (NSDictionary *imageDictionary in images) {
-            [self.imagesArray addObject:[imageDictionary objectForKey:@"url_m"]];
+            }];
+                [self.myCollectionView reloadData];
+
+            }
+            
         }
-        [self.myCollectionView reloadData];
 
     }];
     
+//    self.imagesArray = [[NSMutableArray alloc] init];
+//
+//    NSString *searchTerm = @"Dogs";
+//    NSString *searchString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9401ce3d1573537ff059cca44fe122f4&text=%@&content_type=1&extras=url_m&per_page=10&format=json&nojsoncallback=1", searchTerm];
+//    NSURL *url = [NSURL URLWithString:searchString];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+//        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+//        NSArray *images = [[json objectForKey:@"photos"] objectForKey:@"photo"];
+//        for (NSDictionary *imageDictionary in images) {
+//            [self.imagesArray addObject:[imageDictionary objectForKey:@"url_m"]];
+//        }
+//        [self.myCollectionView reloadData];
+//
+//    }];
+
 }
 
 - (void)save
@@ -91,39 +134,29 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.imagesArray.count;
+    return self.initialArray.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"myCell" forIndexPath:indexPath];
-    cell.alpha = 1.0;
-    NSURL *url = [NSURL URLWithString:[self.imagesArray objectAtIndex:indexPath.row]];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        cell.theImageView.image = [UIImage imageWithData:data];
-    }];
-    
-    if ([self.favoritesArray containsObject:[self.imagesArray objectAtIndex:indexPath.row]]) {
-        cell.theImageView.alpha = 0.5;
-    } else {
-        cell.theImageView.alpha = 1.0;
-    }
-    
+    UIImage *initialImage = [self.initialArray objectAtIndex:indexPath.row];
+    cell.theImageView.image = initialImage;
+    [cell setNeedsLayout];
     return cell;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    PhotoCollectionViewCell *cell = (PhotoCollectionViewCell *) [collectionView cellForItemAtIndexPath:indexPath];
-    if ([self.favoritesArray containsObject:[self.imagesArray objectAtIndex:indexPath.row]]) {
-        cell.theImageView.alpha = 1.0;
-        [self.favoritesArray removeObject:[self.imagesArray objectAtIndex:indexPath.row]];
-    } else {
-        cell.theImageView.alpha = 0.5;
-        [self.favoritesArray addObject:[self.imagesArray objectAtIndex:indexPath.row]];
-    }
-    [self save];
+//    PhotoCollectionViewCell *cell = (PhotoCollectionViewCell *) [collectionView cellForItemAtIndexPath:indexPath];
+//    if ([self.favoritesArray containsObject:[self.imagesArray objectAtIndex:indexPath.row]]) {
+//        cell.theImageView.alpha = 1.0;
+//        [self.favoritesArray removeObject:[self.imagesArray objectAtIndex:indexPath.row]];
+//    } else {
+//        cell.theImageView.alpha = 0.5;
+//        [self.favoritesArray addObject:[self.imagesArray objectAtIndex:indexPath.row]];
+//    }
+//    [self save];
 }
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
@@ -151,26 +184,30 @@
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.imagesArray.count;
+    return self.searchResults.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCell"];
-    cell.alpha = 1.0;
-    NSURL *url = [NSURL URLWithString:[self.imagesArray objectAtIndex:indexPath.row]];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        cell.imageView.image = [UIImage imageWithData:data];
-        //below line fixed the whole bug issue in not displaying the images when reloading.
-        [cell setNeedsLayout];
-    }];
 
-    if ([self.favoritesArray containsObject:[self.imagesArray objectAtIndex:indexPath.row]]) {
-        cell.imageView.alpha = 0.5;
-    } else {
-        cell.imageView.alpha = 1.0;
-    }
+    NSString *username = [self.searchResults objectAtIndex:indexPath.row];
+    cell.textLabel.text = username;
+    [cell setNeedsLayout];
+//    cell.alpha = 1.0;
+//    NSURL *url = [NSURL URLWithString:[self.imagesArray objectAtIndex:indexPath.row]];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+//        cell.imageView.image = [UIImage imageWithData:data];
+//        //below line fixed the whole bug issue in not displaying the images when reloading.
+//        [cell setNeedsLayout];
+//    }];
+//
+//    if ([self.favoritesArray containsObject:[self.imagesArray objectAtIndex:indexPath.row]]) {
+//        cell.imageView.alpha = 0.5;
+//    } else {
+//        cell.imageView.alpha = 1.0;
+//    }
     return cell;
 }
 @end
