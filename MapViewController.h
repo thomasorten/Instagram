499822d5f23f4267c29211cec1075d679982a9c9
rsctiltz <rsctiltz@gmail.com>
2014-06-12 02:10:18 -0500

    Added all profile buttons to link user to different parts of account and added map view, need to connect

---
+++ MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Instagram
+//
+//  Created by Ryan Tiltz on 6/12/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MapViewController : ViewController
+
+@end
