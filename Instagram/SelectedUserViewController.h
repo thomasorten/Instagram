6c846c3ce02048ffe50cc30fb9fb60ce6a239b92
Philipe Castilho <cpcastilho11@outlook.com>
2014-06-12 14:03:49 -0500

    fixed the upload button

---
+++ Instagram/SelectedUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectedUserViewController.h
+//  Instagram
+//
+//  Created by Philipe Castilho on 6/12/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SelectedUserViewController : ViewController
+
+@end
---
+++ Instagram/SelectedUserViewController.m
@@ -0,0 +1,49 @@
+//
+//  SelectedUserViewController.m
+//  Instagram
+//
+//  Created by Philipe Castilho on 6/12/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import "SelectedUserViewController.h"
+
+@interface SelectedUserViewController ()
+
+@end
+
+@implementation SelectedUserViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
