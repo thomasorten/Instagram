8baff4db848bf04ddb0b8a2f6de9875f531d59d2
rsctiltz <rsctiltz@gmail.com>
2014-06-12 13:22:26 -0500

    updated icon and segues

---
+++ Instagram/CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.h
+//  Instagram
+//
+//  Created by Thomas Orten on 6/12/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewController : UIViewController
+@property NSString *photoId;
+@end
